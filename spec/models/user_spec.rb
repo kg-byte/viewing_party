9661aabcb4b9b8650ef499e88794fb2118bdc85b
Kim G <xiaole.guo@yahoo.com>
2022-05-15 13:35:23 -0600

    set up friendship associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:parties) }
     it { should have_many(:party_users) }
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
     it { should have_many(:parties).through(:party_users) }
   end
   describe 'validations' do
@@ -11,6 +13,5 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
-    # it { should validate_uniqueness_of(:email).scoped_to(:id).with_message('** Email Already Exists **') }
   end
 end
