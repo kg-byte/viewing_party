88d169e57787663515cb042b591bf91704db82a8
Kim G <xiaole.guo@yahoo.com>
2022-05-23 10:31:24 -0600

    refactor register/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,22 +24,19 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome, #{user.name}!"
     else
       redirect_to '/register'
-      flash[:alert] = 'Error: please enter a name and unique email to register.'
+      flash[:alert] = "#{user.errors.full_messages.to_sentence}"
     end
 end
 
   def login
     user = User.find_by(email: params[:email])
-    if !user 
-      flash[:error] = "u don goofed try again!"
+    if !user || !user.authenticate(params[:password])
+      flash[:error] = "Incorrect Credentials. Please try again!"
       render :login_form
     elsif user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(user)
-    else
-      flash[:error] = "u don goofed try again!"
-      render :login_form
     end
   end
 
