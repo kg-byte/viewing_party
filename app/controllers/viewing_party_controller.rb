6a71ee21aa73ab13c5dd26343fef883dab7a8071
Kim G <xiaole.guo@yahoo.com>
2022-05-24 09:05:24 -0600

    refactor and make loggedin? require_user

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -28,30 +28,4 @@ class ViewingPartyController < ApplicationController
     end
   end
 
-  private
-
-  def party_params
-    params.permit(:movie_id, :duration, :data, :time)
-  end
-
-  def party_proper_params
-    party_params = {}
-    party_params[:time] = time_format
-    party_params[:movie_id] = params[:movie_id]
-    party_params[:duration] = params[:duration_user]
-    party_params
-  end
-
-  def time_format
-    params['time(2i)'] = '0' + params['time(2i)'] if params['time(2i)'].length == 1
-    time_string = params['time(1i)'] + params['time(2i)'] + params['time(3i)'] + params['time(4i)'] + params['time(3i)']
-  end
-
-  def set_movie
-    @movie = MovieFacade.movie_details(params[:movie_id])
-  end
-
-  def friend_ids
-    params[:friend_ids].map { |id| id.to_i } if params[:friend_ids]
-  end
 end
