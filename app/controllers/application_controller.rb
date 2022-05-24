5f51827a89c48e1c0d8ad909da8c6286a6857900
Kim G <xiaole.guo@yahoo.com>
2022-05-23 20:31:44 -0600

    fix admin error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
   def current_user
-  	@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  	@current_user ||= User.find_by(id: session[:user_id]["value"]) if session[:user_id]
   end
   def current_admin?
   	current_user && current_user.admin?
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe 'login' do
   end
 
   it 'allows admin to view admin dashboard' do 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     details = JSON.parse(File.read('spec/fixtures/movie_details.json'), symbolize_names: true)
     allow(TmdbService).to receive(:movie_details).and_return(details)
     party = create(:party, duration: 200, time: '209901011930') 
@@ -75,7 +75,6 @@ RSpec.describe 'login' do
   end
 
   it 'allows admin to delete a viewing party' do 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     details = JSON.parse(File.read('spec/fixtures/movie_details.json'), symbolize_names: true)
     allow(TmdbService).to receive(:movie_details).and_return(details)
     party = create(:party, duration: 200, time: '209901011930') 
